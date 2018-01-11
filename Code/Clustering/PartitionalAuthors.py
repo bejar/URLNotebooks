ec0ebae77d0f6cdbb0d939cf6bf61d444bf2dafb
javier.bejar <bejar@cs.upc.edu>
2018-01-11 10:25:47 +0100

    Improving Notebooks and example code

--- Code/Clustering/PartitionalAuthors.py
+++ Code/Clustering/PartitionalAuthors.py
@@ -1,10 +1,10 @@
 """
-.. module:: HierarchicalAuthors
+.. module:: PartitionalAuthors
 
-HierarchicalAuthors
+PartitionalAuthors
 *************
 
-:Description: HierarchicalAuthors
+:Description: PartitionalAuthors
 
     
 
@@ -13,7 +13,7 @@ HierarchicalAuthors
 
 :Version: 
 
-:Created on: 18/09/2015 9:35 
+:Created on: 18/09/2015 9:58 
 
 """
 
@@ -22,13 +22,30 @@ __author__ = 'bejar'
 from os import listdir
 from os.path import join
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 from pylab import *
 from sklearn.metrics import adjusted_mutual_info_score
+from sklearn.cluster import KMeans, SpectralClustering
+from sklearn.mixture import GaussianMixture
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
 import argparse
 
-
 path = '/home/bejar/Data/authors/'
+
+
+def show_figure(fdata, labels1, labels2, title='', subtitle=['', '']):
+    fig = plt.figure(figsize=(12, 10))
+    fig.suptitle(title, fontsize=32)
+    ax = fig.add_subplot(121, projection='3d')
+    ax.set_title(subtitle[0])
+    plt.scatter(fdata[:, 0], fdata[:, 1], zs=fdata[:, 2], depthshade=False, c=labels1, s=100)
+    # cbar = plt.colorbar(ticks=range(len(ticks)))
+    # cbar.ax.set_yticklabels(ticks)
+    ax = fig.add_subplot(122, projection='3d')
+    ax.set_title(subtitle[1])
+    plt.scatter(fdata[:, 0], fdata[:, 1], zs=fdata[:, 2], depthshade=False, c=labels2, s=100)
+
+    plt.show()
 
 
 def authors_data(method=1, nfeatures=100):
@@ -66,11 +83,11 @@ def authors_data(method=1, nfeatures=100):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', default=1, choices = [1,2], help='Dataset to use (1 or 2)', type=int)
+    parser.add_argument('--data', default=1, choices=[1, 2], help='Dataset to use (1 or 2)', type=int)
     parser.add_argument('--nfeatures', help="Number of features to generate", default=100, type=int)
-    parser.add_argument('--fmethod', help="Method for generating features", default=1, choices=[1,2,3], type=int)
+    parser.add_argument('--fmethod', help="Method for generating features", default=1, choices=[1, 2, 3], type=int)
+    parser.add_argument('--nclusters', help="Number of clusters", default=2, type=int)
     args = parser.parse_args()
-
 
     if args.data == 1:
         docpath = path + 'Auth1/'
@@ -80,49 +97,36 @@ if __name__ == '__main__':
     nfeatures = args.nfeatures
     method = args.fmethod
 
+    nclusters = args.nclusters
+
     authors, alabels = authors_data(method, nfeatures)
 
-    ### Ward
-    clust = linkage(authors, method='ward')
-    fig = plt.figure(figsize=(8, 8))
-    fig.suptitle('Ward', fontsize=32)
-    dendrogram(clust)
-    plt.show()
-    labels = fcluster(clust, 2, criterion='maxclust')
+    pca = PCA()
+    fdata = pca.fit_transform(authors)
+
+    # KMeans
+    km = KMeans(n_clusters=nclusters, n_init=10, random_state=0)
+    labels = km.fit_predict(authors)
+
     print(adjusted_mutual_info_score(alabels, labels))
 
-    ### Single
-    clust = linkage(authors, method='single')
-    fig = plt.figure(figsize=(8, 8))
-    fig.suptitle('Single Link', fontsize=32)
-    dendrogram(clust)
-    plt.show()
-    labels = fcluster(clust, 2, criterion='maxclust')
+    show_figure(fdata, alabels, labels, subtitle=['Orig', 'K-Means'])
+
+    # GMM
+    # covariance_type = spherical, tied, diag, full
+    gmm = GaussianMixture(n_components=nclusters, covariance_type='full', random_state=0)
+    gmm.fit(authors)
+
+    labels = gmm.predict(authors)
+
     print(adjusted_mutual_info_score(alabels, labels))
 
-    ### Complete
-    clust = linkage(authors, method='complete')
-    fig = plt.figure(figsize=(8, 8))
-    fig.suptitle('Complete Link', fontsize=32)
-    dendrogram(clust)
-    plt.show()
-    labels = fcluster(clust, 2, criterion='maxclust')
+    show_figure(fdata, alabels, labels, subtitle=['Orig', 'GMM'])
+
+    # Spectral Clustering
+    spec = SpectralClustering(n_clusters=nclusters, affinity='nearest_neighbors', n_neighbors=15, random_state=0)
+    labels = spec.fit_predict(authors)
+
     print(adjusted_mutual_info_score(alabels, labels))
 
-    ### Average
-    clust = linkage(authors, method='average')
-    fig = plt.figure(figsize=(8, 8))
-    fig.suptitle('Average', fontsize=32)
-    dendrogram(clust)
-    plt.show()
-    labels = fcluster(clust, 2, criterion='maxclust')
-    print(adjusted_mutual_info_score(alabels, labels))
-
-    ### Centroid
-    clust = linkage(authors, method='centroid')
-    fig = plt.figure(figsize=(8, 8))
-    fig.suptitle('Centroid', fontsize=32)
-    dendrogram(clust)
-    plt.show()
-    labels = fcluster(clust, 2, criterion='maxclust')
-    print(adjusted_mutual_info_score(alabels, labels))
+    show_figure(fdata, alabels, labels, subtitle=['Orig', 'Spectral'])
